9339b1b1e51491451593832789f6e11d3e454ac2
parth sharma <parthsharma1011@gmail.com>
2025-11-30 18:49:36 +0100

    Add configuration file with API key and Google Sheets URL

---
+++ genai_folder/utils/config.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+class Config:
+    GEMINI_API_KEY = ""
+    SHEET_URL = "https://docs.google.com/spreadsheets/d/12ckgXOMUoTzuPYbIXYe3TTcWuwxvGvUk/edit?usp=sharing&ouid=102910574304767206169&rtpof=true&sd=true"
